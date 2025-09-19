cd204a33e53e497ef48ede5ed2ac2f6d3b400ed9
RomanGit777 <rsavenko91@gmail.com>
2025-09-19 18:41:27 +0200

    hw1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import './App.css'
+
+const coursesTitleArray: string[] = [
+    'JavaScript Complex',
+    'Java Complex',
+    'Python Complex',
+    'QA Complex',
+    'Fullstack',
+    'Frontend'
+];
 function App() {
   return (
       <>
+          <ul>
+          {
+              coursesTitleArray.map((course, index) =>  <li key={index}>  {course}</li>)
+          }
+          </ul>
       </>  );
 }
 
