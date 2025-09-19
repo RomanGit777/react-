514942d1384c847614d251e9ac355bd2aacfbcd3
RomanGit777 <rsavenko91@gmail.com>
2025-09-19 18:31:18 +0200

    master branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import './App.css'
-import {products} from "./data/productsList.ts";
-import MyProduct from "./MyProduct/MyProduct.tsx";
-
 function App() {
   return (
       <>
-          {products.map((product, index) => <MyProduct key={index} product={product}/>
-
-          )};
-
       </>  );
 }
 
