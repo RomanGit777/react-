7ec00487aaf49054ac6f05a33d3740bc0ad40e07
RomanGit777 <rsavenko91@gmail.com>
2025-09-25 21:46:52 +0200

    lesson 2/ working with useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import './App.css'
+import {useState} from "react";
 function App() {
-  return (
-      <>
-      </>  );
+
+    // eslint-disable-next-line prefer-const
+    let [counter, setCounter] = useState<number>(0);
+    console.log('mount');
+    return (
+             <div>
+                <h2>{counter}</h2>
+
+                 <button onClick={() => {
+                     setCounter(++counter);
+                 }}>increment
+                 </button>
+
+                 <button onClick={() => {
+                     setCounter(--counter);
+                 }}>decrement
+                 </button>
+
+             </div>
+)
+    ;
 }
 
 export default App
