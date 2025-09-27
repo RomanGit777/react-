3ecdd7029b8f9d03cc6e59019a79d250195d207d
RomanGit777 <rsavenko91@gmail.com>
2025-09-27 12:43:01 +0200

    lesson 2/ useEffect fetch+services for Users

--- src/services/api.services.ts
+++ src/services/api.services.ts
@@ -1,10 +1,10 @@
 import type {IUser} from "../models/IUser.ts";
 
 export const getUsers = async (): Promise<IUser[]> => {
-    return await fetch('https://jsonplaceholder.typicode.com/users')
+    return await fetch(import.meta.env.VITE_API_URL)
         .then(res => res.json())
 }
 export const getUser = async (id:string): Promise<IUser> => {
-    return await fetch('https://jsonplaceholder.typicode.com/users/' + id)
+    return await fetch(import.meta.env.VITE_API_URL + '/' + id)
         .then(res => res.json())
 }
