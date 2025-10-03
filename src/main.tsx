acc0c2a55c1030ef416d58a28a5d4bfa2cd90d3f
RomanGit777 <rsavenko91@gmail.com>
2025-10-03 12:37:58 +0200

    lesson/lesson3/ router demo/ add changes to package.json and main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(<App/>)
+const routes = createBrowserRouter([
+    {index: true, element: <Layout/>}
+])
+createRoot(document.getElementById('root')!)
+    .render(<RouterProvider router={routes}/>)
