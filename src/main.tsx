914e8fbc81782676abd57a1bdb593020a66dadd8
RomanGit777 <rsavenko91@gmail.com>
2025-10-13 11:57:02 +0200

    hw/ hw3/ #qL0b5uh

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,8 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import App from './App';
-import {UsersDummyPage} from "./pages/UsersDummyPage.tsx";
-import { UsersJSONPage } from './pages/UsersJSONPage';
-import {UsersPage} from "./pages/UsersPage.tsx";
-import {PostsPage} from "./pages/PostsPage.tsx";
-import { PostsJSONPage } from './pages/PostsJSONPage';
-import {PostsDummyPage} from "./pages/PostsDummyPage.tsx";
-import {CommentsPage} from "./pages/CommentsPage.tsx";
+import {RouterProvider} from "react-router-dom";
+import {routes} from "./router/Routes.tsx";
 
-const routes = createBrowserRouter([
-    {path: '', element: <App/>, children:[
-            {path:'users',element:<UsersPage/>, children:[
-                    {path:'jsonplaceholder', element: <UsersJSONPage/>},
-                    {path:'dummyjson', element: <UsersDummyPage/>}
-                ]},
-            {path:'posts',element:<PostsPage/>, children:[
-                    {path:'jsonplaceholder', element: <PostsJSONPage/>},
-                    {path:'dummyjson', element: <PostsDummyPage/>}
-                ]},
-            {path:'comments/jsonplaceholder',element:<CommentsPage/>},
-        ]}
-])
 
 createRoot(document.getElementById('root')!)
     .render(<RouterProvider router={routes}/>)
---
+++ src/router/Routes.tsx
@@ -0,0 +1,23 @@
+import {createBrowserRouter} from "react-router-dom";
+import App from "../App.tsx";
+import {UsersPage} from "../pages/UsersPage.tsx";
+import {UsersJSONPage} from "../pages/UsersJSONPage.tsx";
+import {UsersDummyPage} from "../pages/UsersDummyPage.tsx";
+import { PostsPage } from "../pages/PostsPage.tsx";
+import { PostsJSONPage } from "../pages/PostsJSONPage.tsx";
+import { CommentsPage } from "../pages/CommentsPage.tsx";
+import {PostsDummyPage} from "../pages/PostsDummyPage.tsx";
+
+     export const routes = createBrowserRouter([
+        {path: '', element: <App/>, children:[
+                {path:'users',element:<UsersPage/>, children:[
+                        {path:'jsonplaceholder', element: <UsersJSONPage/>},
+                        {path:'dummyjson', element: <UsersDummyPage/>}
+                    ]},
+                {path:'posts',element:<PostsPage/>, children:[
+                        {path:'jsonplaceholder', element: <PostsJSONPage/>},
+                        {path:'dummyjson', element: <PostsDummyPage/>}
+                    ]},
+                {path:'comments/jsonplaceholder',element:<CommentsPage/>},
+            ]}
+    ])
