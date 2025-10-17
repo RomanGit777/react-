8a9dba8681a788dccc04212010ccabf6e3f46ff2
RomanGit777 <rsavenko91@gmail.com>
2025-10-17 12:59:15 +0200

    hw/ hw3/ #tntlwuxFxI5/ done

--- src/components/post/PostComponent.tsx
+++ src/components/post/PostComponent.tsx
@@ -1,5 +1,15 @@
-export const PostComponent = () => {
+import type {IPost} from "../../models/IPost.ts";
+
+interface PostComponentProps {
+    posts: IPost
+}
+
+export const PostComponent = ({posts}: PostComponentProps) => {
     return (
-        <></>
+        <>
+            <div className={'m-3.5'}>
+             <p>{posts.title}</p>
+            </div>
+        </>
     );
 };
--- src/components/posts/PostsComponent.tsx
+++ src/components/posts/PostsComponent.tsx
@@ -1,7 +1,18 @@
 import {PostComponent} from "../post/PostComponent.tsx";
+import type {IPost} from "../../models/IPost.ts";
+import {useEffect, useState} from "react";
+import {getAll} from "../../services/general.api.service.ts";
+import type {IBaseResponseModel} from "../../models/IBaseResponseModel.ts";
 
 export const PostsComponent = () => {
+
+    const [posts, setPosts] = useState<IPost[]>([]);
+    useEffect(() => {
+        getAll<IBaseResponseModel & {posts: IPost[] }>('/posts').then(({posts})=>setPosts(posts));
+    }, [])
+
     return (
-        <><PostComponent/></>
+        <> {posts.map((posts:IPost) => <PostComponent key={posts.id} posts={posts}/>)}
+        </>
     );
 };
