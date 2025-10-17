969836ba5736830aecfc0b65dcb5d14ca5a7711b
RomanGit777 <rsavenko91@gmail.com>
2025-10-17 11:50:51 +0200

    hw/ hw3/ #tntlwuxFxI5/ routes for posts and users

--- src/Layout/MainLayout.tsx
+++ src/Layout/MainLayout.tsx
@@ -1,9 +1,10 @@
 import {Outlet} from "react-router/internal/react-server-client";
+import { Menu } from "../menu/Menu";
 
 export const MainLayout = () => {
     return (
         <>
-        Main Layout
+        <Menu/>
         <Outlet/>
         </>
     );
---
+++ src/menu/Menu.tsx
@@ -0,0 +1,10 @@
+import {Link} from "react-router";
+
+export const Menu = () => {
+    return (
+        <ul>
+            <li><Link to={'users'}>users</Link></li>
+            <li><Link to={'posts'}>posts</Link></li>
+        </ul>
+    );
+};
