082d07793cdd867162c968431d8a57d3f1a2eba4
RomanGit777 <rsavenko91@gmail.com>
2025-10-19 12:25:27 +0200

    hw/ hw3/ #z5PFSTAK/ add menu

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,8 +1,10 @@
 import {Outlet} from "react-router";
+import {Menu} from "../menu/Menu.tsx";
 
 export const MainLayout = () => {
     return (
         <>
+            <Menu/>
             <Outlet/>
         </>
     );
---
+++ src/menu/Menu.tsx
@@ -0,0 +1,9 @@
+import {Link} from "react-router";
+
+export const Menu = () => {
+    return (
+        <ul>
+        <li><Link to={'users'}>user page</Link></li>
+        </ul>
+    );
+};
