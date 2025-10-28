f14905687a41a7e53a69ca2b59d6c07002208e28
RomanGit777 <rsavenko91@gmail.com>
2025-10-28 11:46:08 +0100

    lessons/ lesson 6/ authentication/ service for login

--- src/components/login-component/LoginComponent.tsx
+++ src/components/login-component/LoginComponent.tsx
@@ -1,5 +1,17 @@
+import {useEffect} from "react";
+import {login} from "../../services/api.service.ts";
+
 export const LoginComponent = () => {
+
+    useEffect(() => {
+        login({
+            username: 'emilys',
+            password: 'emilyspass',
+            expiresInMins: 1,
+        })
+    }, []);
+
     return (
-        <>dfdfdf</>
+        <>Login Component</>
     );
 };
---
+++ src/models/IUserWithTokens.ts
@@ -0,0 +1,12 @@
+export interface IUserWithTokens {
+	firstName: string;
+	lastName: string;
+	image: string;
+	gender: string;
+	id: number;
+	accessToken: string;
+	email: string;
+	username: string;
+	refreshToken: string;
+}
+
