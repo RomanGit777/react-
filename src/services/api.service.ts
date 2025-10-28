6cf13a8aa54a3a85f1b316505ed857b787e688a9
RomanGit777 <rsavenko91@gmail.com>
2025-10-28 10:53:02 +0100

    lessons/ lesson 6/ authentication/ add refresh

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -3,6 +3,7 @@ import type {IUserWithTokens} from "../models/IUserWithTokens.ts";
 import type {IProduct} from "../models/IProduct.ts";
 import type {ProductBaseResponseModelType} from "../models/ProductBaseResponseModelType.ts";
 import {retrieveLocalStorage} from "./helpers.ts";
+import type {ITokenPair} from "../models/ITokenPair.ts";
 type LoginData = {
     username: string;
     password: string;
@@ -31,4 +32,15 @@ export const login = async ({username, password, expiresInMins}: LoginData): Pro
 export const loadAuthProducts = async ():Promise<IProduct[]> => {
     const {data:{products}} = await axiosInstance.get<ProductBaseResponseModelType>('/products');
     return products;
+}
+
+export const refresh = async () => {
+    const userWithTokens = retrieveLocalStorage<IUserWithTokens>('user');
+    const {data: {refreshToken,accessToken} } = await axiosInstance.post<ITokenPair>('/refresh',
+        {refreshToken: userWithTokens.refreshToken,
+        expiresInMins: 1
+        })
+            userWithTokens.accessToken = accessToken;
+            userWithTokens.refreshToken = refreshToken;
+            localStorage.setItem('user', JSON.stringify(userWithTokens));
 }
