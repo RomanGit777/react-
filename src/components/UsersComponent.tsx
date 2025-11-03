74a8823321c78002646b01e3636bbde61e33d783
RomanGit777 <rsavenko91@gmail.com>
2025-11-03 12:27:43 +0100

    lessons/ lesson 7/ useMemo

--- src/components/UsersComponent.tsx
+++ src/components/UsersComponent.tsx
@@ -1,10 +1,13 @@
 import {UserComponent} from "./UserComponent.tsx";
-import {useCallback, useEffect, useState} from "react";
+import {useCallback, useEffect, useMemo, useState} from "react";
 
 export const UsersComponent = () => {
     console.log('users')
     const [users, setUsers] = useState()
 
+    const arr:number[] = useMemo(() => {
+        return [11,22,33];
+    } ,[])
     const foo = useCallback(() => {
         console.log('test')
     }, [])
@@ -17,7 +20,7 @@ export const UsersComponent = () => {
     return (
         <div>
             users
-            <UserComponent foo={foo}/>
+            <UserComponent foo={foo} arr={arr}/>
         </div>
     );
 };
