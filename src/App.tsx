7d31c64187599ec4de1823fa6081dcc3e441c173
RomanGit777 <rsavenko91@gmail.com>
2025-11-04 13:58:55 +0100

    hw/ hw 7/ GP0nA6rxivp

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
 import {A} from "./components/A.tsx";
 import {B} from "./components/B.tsx";
+import {MyContext} from "./context/MyContext.tsx";
+import {useState} from "react";
+
 function App() {
-  return (
+    const [theme, setTheme] = useState<string>('light');
+    return (
       <>
+          <MyContext.Provider value={{
+              theme: theme,
+              changeTheme: (themeValue: string) => setTheme(themeValue)
+          }}>
           <A/>
           <B/>
+          </MyContext.Provider>
       </>
   );
 }
--- src/components/AA.tsx
+++ src/components/AA.tsx
@@ -1,5 +1,11 @@
+import './AA.css'
+import {useContext} from "react";
+import {MyContext} from "../context/MyContext.tsx";
 export const AA = () => {
+    const {theme} = useContext(MyContext)
     return (
-        <></>
+        <div className={theme}>
+            this is AA component and color value now is - {theme}
+        </div>
     );
 };
