8b0a337db182b286ea8ac9d7fc9992673e559628
RomanGit777 <rsavenko91@gmail.com>
2025-11-04 13:19:19 +0100

    hw/ hw 7/ GP0nA6rxivp/ base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import './App.css'
+import {A} from "./components/A.tsx";
+import {B} from "./components/B.tsx";
 function App() {
   return (
       <>
-      </>  );
+          <A/>
+          <B/>
+      </>
+  );
 }
 
 export default App
