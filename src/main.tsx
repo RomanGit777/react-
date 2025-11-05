ec7fb20b9f0a15d48991cfb31fcd5b00baef2784
RomanGit777 <rsavenko91@gmail.com>
2025-11-05 11:54:21 +0100

    lesson/ lesson 8/ redux/ provider, slice, type, initial state

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,32 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import {RouterProvider} from "react-router-dom";
 import {routes} from "./router/Routes.tsx";
+import {Provider} from "react-redux";
+import {configureStore, createSlice} from "@reduxjs/toolkit";
+import type {IUserModel} from "./models/IUserModel.ts";
+
+type userSliceType = {
+    users: IUserModel[];
+}
+
+const initialState: userSliceType = {users: []}
+
+const userSlice = createSlice({
+    name: "userSlice", // name of our slice
+    initialState: initialState,
+    reducers: {} // there will be a function here that will affect the state
+});
+
+const store = configureStore({ // this creates a redux store - a single place where all your app's state lives
+    reducer: {
+        userSlice: userSlice.reducer
+        // postSlice: null
+    }
+});
 
 createRoot(document.getElementById('root')!)
-    .render(<RouterProvider router={routes}/>)
+    .render(        // The Provider makes the redux store available for all components in router
+        <Provider store={store}>
+            <RouterProvider router={routes}/>
+        </Provider>
+    )
