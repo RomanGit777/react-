ce786f762447b69a729fae7a0ad2adba61618df5
RomanGit777 <rsavenko91@gmail.com>
2025-11-05 13:36:14 +0100

    lesson/ lesson 8/ redux/ dispatch+uSA

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -8,13 +8,13 @@ export const UsersPage = () => {
     // useAppSelector is a typed hook that extracts data from the Redux store;
     const {users} = useAppSelector(({userSlice}) => userSlice);
     // here, we’re selecting the userSlice portion of the state.
-    const dispatch = useDispatch();
+    const dispatch = useDispatch(); // makes it possible to connect functions from the store
     useEffect(() => {
         fetch('http://jsonplaceholder.typicode.com/users')
             .then(res => res.json())
             .then(users => {
                 dispatch(userSliceActions.loadUsers(users)); // now initial state changed, because this arg "users"
-                // inside state arg in func
+                // is here instead of action, and action gives info back to state
             })
     }, []);
     return (
