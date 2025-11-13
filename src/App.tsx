26b55135ec420e712f2cd4cc97cb678b5dc58c78
RomanGit777 <rsavenko91@gmail.com>
2025-11-13 13:52:43 +0100

    tanStack Start/ base structure + fetched data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import './App.css'
+import axios from "axios";
+import {useQuery} from "@tanstack/react-query";
+
+const fetchTodos = () => {
+    return axios.get("https://jsonplaceholder.typicode.com/todos").then(res => res.data)
+}
+
 function App() {
-  return (
-      <>
-      </>  );
+
+
+    const {data,error,isLoading} = useQuery({
+        queryKey: ['todos'],
+        queryFn: fetchTodos
+    });
+
+    if(isLoading) return <div>Loading...</div>
+    if(isLoading) return <div>Error: {error}</div>
+
+    return (
+      <div>
+          {
+              data.map((todo) =>
+              <li key={todo.id}>{todo.title}</li>
+              )
+          }
+      </div>  );
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(<App/>)
+// This provides a centralized store for all your queries.
+// Think of it like Redux but for server data.
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')!)
+    .render(
+    <QueryClientProvider client={queryClient}>
+        <App/>
+    </QueryClientProvider>
+    )
